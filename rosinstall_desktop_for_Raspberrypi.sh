42e96478d00a2001125171b4621df238414c45e0
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-06-19 09:38:27 +0900

    Add files via upload

---
+++ rosinstall_desktop_for_Raspberrypi.sh
@@ -0,0 +1,34 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt update
+
+sudo apt install -y python3-rosdep python3-rosinstall-generator python3-wstool python3-rosinstall build-essential cmake
+
+sudo rosdep init
+
+rosdep update
+
+mkdir -p ~/catkin_ws
+
+cd ~/catkin_ws
+
+rosinstall_generator desktop --rosdistro noetic --deps --wet-only --tar > noetic-desktop-wet.rosinstall
+
+wstool init src noetic-desktop-wet.rosinstall
+
+
+cd ~/catkin_ws
+
+rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
+
+sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
+
+nano /etc/dphys-swapfile/ CONF_SWAPSIZE=1024 *1024 depends on the RAM on your raspberry pi )
+
+source /opt/ros/noetic/setup.bash
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
