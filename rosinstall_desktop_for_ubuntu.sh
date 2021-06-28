178f89257c2495e17315960c3a0e368259916754
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-06-28 11:40:00 +0900

    Update rosinstall_desktop_for_ubuntu.sh

--- rosinstall_desktop_for_ubuntu.sh
+++ rosinstall_desktop_for_ubuntu.sh
@@ -8,6 +8,8 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 
+sudo apt update
+
 sudo apt install ros-noetic-desktop
 
 sudo apt install ros-noetic-map-server
