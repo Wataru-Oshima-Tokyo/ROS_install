8053f1a38d334d74b2999a09636c091c19d883a1
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-07-09 16:25:01 +0900

    Update ros(melodic)install_desktop_ubuntu.sh

--- ros(melodic)install_desktop_ubuntu.sh
+++ ros(melodic)install_desktop_ubuntu.sh
@@ -1,1 +1,28 @@
+
+sudo apt update
+
+sudo apt install curl
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install ros-melodic-desktop
+
+sudo apt install ros-melodic-map-server
+
+sudo apt install ros-melodic-gazebo-ros
+
+sudo apt install ros-melodic-cv-bridge
+
+sudo apt install ros-melodic-camera-info-manager
+
+source /opt/ros/melodic/setup.bash
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
