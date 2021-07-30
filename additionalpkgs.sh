cff0638036fd038e777ca11f2e8fb43491971cf2
you@example.com <you@example.com>
2021-07-31 01:37:16 +0900

    added some more

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -1,13 +1,3 @@
- git clone https://github.com/ros-perception/image_pipeline 
- rm -r vision_opencv -y
- git clone https://github.com/ros-perception/vision_opencv -b melodic
- git clone https://github.com/ros-perception/geometry2
- git clone https://github.com/ros-planning/navigation
- git clone https://github.com/stonier/ecl_core -b release/0.62-melodic
- git clone https://github.com/stonier/ecl_lite -b release/0.61-melodic
- git clone https://github.com/stonier/ecl_tools -b release/0.61-melodic
- git clone https://github.com/stonier/sophus -b release/1.1.x
- git clone https://github.com/Wataru-Oshima-Tokyo/orne_tools
- git clone https://github.com/Wataru-Oshima-Tokyo/orne_rviz_plugins
- git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main
- 
+git clone https://github.com/Wataru-Oshima-Tokyo/orne_tools
+git clone https://github.com/Wataru-Oshima-Tokyo/orne_rviz_plugins
+git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main 
--- ros-melodic-install_desktop_Raspberrypi.sh
+++ ros-melodic-install_desktop_Raspberrypi.sh
@@ -21,14 +21,17 @@ wstool init src melodic-desktop-wet.rosinstall
 cd ~/catkin_ws/src
 
 git clone https://github.com/ros-perception/image_pipeline 
-rm -r vision_opencv -y
+sudo rm -r vision_opencv
 git clone https://github.com/ros-perception/vision_opencv -b melodic
 git clone https://github.com/ros-perception/geometry2
+git clone https://github.com/ros-perception/pcl_ros
 git clone https://github.com/ros-planning/navigation
 git clone https://github.com/stonier/ecl_core -b release/0.62-melodic
 git clone https://github.com/stonier/ecl_lite -b release/0.61-melodic
 git clone https://github.com/stonier/ecl_tools -b release/0.61-melodic
 git clone https://github.com/stonier/sophus -b release/1.1.x
+sudo rm -r image_common
+git clone https://github.com/ros-perception/image_common -b hydro-devel
 
 cd ~/catkin_Ws
 
@@ -43,3 +46,5 @@ source /opt/ros/melodic/setup.bash
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 
 source ~/.bashrc
+
+echo "If you cannot build it properly, you need to upgrde the gcc. pls check this http://suka4.blogspot.com/2016/12/raspberry-pi-3-gcc6.html"
