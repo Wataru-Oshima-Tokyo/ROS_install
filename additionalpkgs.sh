48cd77e67fcfc1302193246484ed1ee2e6d68620
you@example.com <you@example.com>
2021-07-31 00:49:30 +0900

    added some packages

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -1,8 +1,13 @@
  git clone https://github.com/ros-perception/image_pipeline 
- git clone https://github.com/ros-perception/pcl-ros
+ rm -r vision_opencv -y
+ git clone https://github.com/ros-perception/vision_opencv -b melodic
  git clone https://github.com/ros-perception/geometry2
  git clone https://github.com/ros-planning/navigation
  git clone https://github.com/stonier/ecl_core -b release/0.62-melodic
  git clone https://github.com/stonier/ecl_lite -b release/0.61-melodic
  git clone https://github.com/stonier/ecl_tools -b release/0.61-melodic
  git clone https://github.com/stonier/sophus -b release/1.1.x
+ git clone https://github.com/Wataru-Oshima-Tokyo/orne_tools
+ git clone https://github.com/Wataru-Oshima-Tokyo/orne_rviz_plugins
+ git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main
+ 
--- ros-melodic-install_desktop_Raspberrypi.sh
+++ ros-melodic-install_desktop_Raspberrypi.sh
@@ -18,7 +18,19 @@ rosinstall_generator desktop --rosdistro melodic --deps --wet-only --tar > melod
 
 wstool init src melodic-desktop-wet.rosinstall
 
-cd ~/catkin_ws
+cd ~/catkin_ws/src
+
+git clone https://github.com/ros-perception/image_pipeline 
+rm -r vision_opencv -y
+git clone https://github.com/ros-perception/vision_opencv -b melodic
+git clone https://github.com/ros-perception/geometry2
+git clone https://github.com/ros-planning/navigation
+git clone https://github.com/stonier/ecl_core -b release/0.62-melodic
+git clone https://github.com/stonier/ecl_lite -b release/0.61-melodic
+git clone https://github.com/stonier/ecl_tools -b release/0.61-melodic
+git clone https://github.com/stonier/sophus -b release/1.1.x
+
+cd ~/catkin_Ws
 
 rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
 
