c07e04de0f1dde248f4cfdfe730f2bff075ec22d
you@example.com <you@example.com>
2021-07-30 20:47:07 +0900

    additional pkgs

---
+++ additionalpkgs.sh
@@ -0,0 +1,8 @@
+ git clone https://github.com/ros-perception/image_pipeline 
+ git clone https://github.com/ros-perception/pcl-ros
+ git clone https://github.com/ros-perception/geometry2
+ git clone https://github.com/ros-planning/navigation
+ git clone https://github.com/stonier/ecl_core -b release/0.62-melodic
+ git clone https://github.com/stonier/ecl_lite -b release/0.61-melodic
+ git clone https://github.com/stonier/ecl_tools -b release/0.61-melodic
+ git clone https://github.com/stonier/sophus -b release/1.1.x
