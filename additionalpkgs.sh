236ea4a85793ea60048ce81934637fe5766bd665
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-08-02 14:05:46 +0900

    Update additionalpkgs.sh

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -5,6 +5,8 @@ git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main
 git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
 #git clone https://github.com/ros-perception/perception_pcl -b melodic-devel
 #git clone https://github.com/ros-perception/perception_pcl -b noetic-devel
+git clone https://github.com/IntelRealSense/realsense-ros
+git clone https://github.com/pal-robotics/ddynamic_reconfigure
 
 
 
