71e28056d8d8e54537963b42a0bd35a2084e9cbd
you@example.com <you@example.com>
2021-08-06 13:49:34 +0900

    added some pkgs

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -8,5 +8,10 @@ git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
 git clone https://github.com/IntelRealSense/realsense-ros
 git clone https://github.com/pal-robotics/ddynamic_reconfigure
 
+git clone https://github.com/ros/joint_state_publisher.git 
+git clone https://github.com/ros-controls/ros_controllers
+git clone https://github.com/cyberrobotics/webots_ros
+git clone https://github.com/cyberbotics/webots_ros -b melodic
+git clone https://github.com/ros-controls/ros_control
 
 
