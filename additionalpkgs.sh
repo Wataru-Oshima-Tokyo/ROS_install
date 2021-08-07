12e24925e10b18a6f5c91ce698a1d19145403de5
Your Name <you@example.com>
2021-08-08 01:00:16 +0900

    fixed some bugs

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -9,3 +9,5 @@ git clone https://github.com/Slamtec/rplidar_ros
 #git clone https://github.com/ros-perception/perception_pcl -b noetic-devel
 git clone https://github.com/IntelRealSense/realsense-ros
 git clone https://github.com/pal-robotics/ddynamic_reconfigure
+sudo apt install libudev-dev -y
+
