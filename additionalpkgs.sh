72981c70a78ce3474550a657dc432174259a3c48
Wataru Oshima <woshima@mail.csuchio.edu>
2021-10-05 17:03:28 -0700

    added ros2install

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -2,7 +2,7 @@ cd ~/catkin_ws/src/
 git clone https://github.com/Wataru-Oshima-Tokyo/orne_tools
 git clone https://github.com/Wataru-Oshima-Tokyo/orne_rviz_plugins
 git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main 
-git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
+#git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
 git clone https://github.com/Slamtec/rplidar_ros
 
 #git clone https://github.com/ros-perception/perception_pcl -b melodic-devel
