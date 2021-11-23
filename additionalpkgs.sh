6b85d98e14c143d042912222f58e94bcf6ece387
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2021-11-22 21:47:23 -0800

    fixed  noetic raspi

--- additionalpkgs.sh
+++ additionalpkgs.sh
@@ -2,12 +2,12 @@ cd ~/catkin_ws/src/
 git clone https://github.com/Wataru-Oshima-Tokyo/orne_tools
 git clone https://github.com/Wataru-Oshima-Tokyo/orne_rviz_plugins
 git clone https://github.com/Wataru-Oshima-Tokyo/fulanghua_navigation -b main 
-#git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
-git clone https://github.com/Slamtec/rplidar_ros
+git clone https://github.com/Wataru-Oshima-Tokyo/ldlidar
+#git clone https://github.com/Slamtec/rplidar_ros
 
 #git clone https://github.com/ros-perception/perception_pcl -b melodic-devel
 #git clone https://github.com/ros-perception/perception_pcl -b noetic-devel
-git clone https://github.com/IntelRealSense/realsense-ros
-git clone https://github.com/pal-robotics/ddynamic_reconfigure
-sudo apt install libudev-dev -y
+#git clone https://github.com/IntelRealSense/realsense-ros
+#git clone https://github.com/pal-robotics/ddynamic_reconfigure
+#sudo apt install libudev-dev -y
 
