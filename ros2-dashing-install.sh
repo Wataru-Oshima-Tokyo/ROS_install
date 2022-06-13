d5423b9c8094bbdb78f68941131d6fd9ec614d7d
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-06-13 13:09:15 +0900

    added realsense

--- ros2-dashing-install.sh
+++ ros2-dashing-install.sh
@@ -26,3 +26,9 @@ sudo apt upgrade -y
 
 sudo apt install ros-dashing-desktop -y 
 
+sudo apt-get install ros-dashing-cv-bridge ros-dashing-librealsense2 ros-dashing-message-filters ros-dashing-image-transport -y
+
+sudo apt-get install -y libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
+sudo apt-get install -y libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
+
+sudo apt-get install ros-dashing-realsense-camera-msgs ros-dashing-realsense-ros2-camera -y
