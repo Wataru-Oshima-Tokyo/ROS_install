ae54a3d06c2ef80ca2e68d7e9d8c7d0e6a63ef7a
Wataru Oshima <woshima@pocketradar.com>
2023-01-18 23:36:27 +0000

    added turtlebot3 env and simulation

---
+++ turtlebot_pkg.sh
@@ -0,0 +1,9 @@
+cd $HOME
+mkdir -p catkin/src
+cd catkin_ws/src
+sudo apt install -y ros-melodic-gazebo-dev ros-melodic-gazebo-plugins ros-melodic-gazebo-ros ros-melodic-gazebo-ros-control ros-melodic-gazebo-ros-pkgs
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_gazebo_plugin.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/Wataru-Oshima-Tokyo/turtlebot3_simulations.git
+
