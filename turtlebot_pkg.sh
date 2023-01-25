95c18bd534f9ccb0c6a1a736325a21ecd1c077d7
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-01-25 02:02:53 +0000

    fixed a bug

--- turtlebot_pkg.sh
+++ turtlebot_pkg.sh
@@ -1,5 +1,5 @@
 cd $HOME
-mkdir -p catkin/src
+mkdir -p catkin_ws/src
 cd catkin_ws/src
 sudo apt install -y ros-melodic-gazebo-dev ros-melodic-gazebo-plugins ros-melodic-gazebo-ros ros-melodic-gazebo-ros-control ros-melodic-gazebo-ros-pkgs
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
